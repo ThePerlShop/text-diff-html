c3ce14c326aef2faa907ff0695f6499b08e97c8f
David Wheeler <david@justatheory.com>
2005-09-04 17:08:55 +0000

    Incremented version number to 0.03.

--- lib/Text/Diff/HTML.pm
+++ lib/Text/Diff/HTML.pm
@@ -7,7 +7,7 @@ use vars qw(@ISA $VERSION);
 use HTML::Entities;
 use Text::Diff (); # Just to be safe.
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 @ISA = qw(Text::Diff::Unified);
 
 sub file_header {
